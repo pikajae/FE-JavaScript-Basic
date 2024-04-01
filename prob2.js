5b3ca68dc91105c961308334f10818f5b61b6fad
pikajae <pikajae@inha.edu>
2024-04-01 22:27:50 +0900

    240401 과제 커밋

--- prob2.js
+++ prob2.js
@@ -1,1 +1,27 @@
 /* 2번 문제 답안을 작성해주세요. */
+function fact(input)
+{
+    let out = 0;
+    for (let i = input - 1; 0 < i; i--)
+    {
+        input = input * i;
+    }
+    out = input;
+    return out;
+}
+
+function solution(n)
+{
+    let answer = 0;
+    for (let i = 1; i <= 10; i++)
+    {
+        answer++;
+        let nf=fact(i);
+        let nf1=fact(i+1);
+        if ((nf <= n)&&(n< nf1))
+        {
+            return answer;
+            break;
+        }
+    }
+}
--- prob3.js
+++ prob3.js
@@ -1,1 +1,20 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(arr) {
+    let answer = [];
+    let min = Math.min(...arr); //small obj
+    let min_num = 0; //smallest order
+    let len = arr.length;
+    if (len == 1) {
+        arr=[-1];
+    }
+    else {
+        for (let i = 0; i < len; i++) {
+            if (arr[i] == min) {
+                min_num = i;
+                arr.splice(min_num, 1);
+            }
+        }
+    }
+    answer = arr;
+    return answer;
+}
